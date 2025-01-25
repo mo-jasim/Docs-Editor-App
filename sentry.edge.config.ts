4ba0fed1020250df4a6441c2121bc31b28d29f65
mo-jasim <mrjasim87@gmail.com>
2025-01-25 16:13:26 +0530

    add initial project setup with configuration files, SVG icons, and authentication pages

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e964c8db3b2b9456c0d69e5e48566aa3@o4508703491686400.ingest.us.sentry.io/4508703521832960",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
